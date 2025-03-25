d9a297b6a70278432e132cff342a1be157c1a3b0
jezabel-bacalso <jezabel.melani@gmail.com>
2025-03-25 13:02:00 -0700

    user

---
+++ Bakery/src/User.java
@@ -0,0 +1,39 @@
+// Abstract User class
+abstract class User {
+    private String name;
+    private String email;
+    private String password;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public abstract String getRole();
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        User user = (User) obj;
+        return email.equals(user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return email.hashCode();
+    }
+}
